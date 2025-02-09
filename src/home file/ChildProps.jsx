e9498fee13d590a7a1af21b4de0d5135c141e54f
minhazapon <minhazaponcodingwarrior@gmail.com>
2025-02-08 21:10:13 -0800

    added des file

--- src/home file/ChildProps.jsx
+++ src/home file/ChildProps.jsx
@@ -1,15 +1,18 @@
 
 
 function ChildProps({ aponskills }) {
+
+    const { s1, s2, s3, s4, s5 } = aponskills
+
     return (
         <>
             <div className=" flex justify-center ">
                 <div className=" mt-48 border-[1px] border-cyan-300 w-[300px]  h-[300px] p-20">
-                    <p className=" text-xl font-serif text-cyan-400"> {aponskills.s1} </p>
-                    <p className=" text-xl font-serif text-cyan-400"> {aponskills.s2} </p>
-                    <p className=" text-xl font-serif text-cyan-400"> {aponskills.s3} </p>
-                    <p className=" text-xl font-serif text-cyan-400"> {aponskills.s4} </p>
-                    <p className=" text-xl font-serif text-cyan-400"> {aponskills.s5} </p>
+                    <p className=" text-xl font-serif text-cyan-400"> {s1} </p>
+                    <p className=" text-xl font-serif text-cyan-400"> {s2} </p>
+                    <p className=" text-xl font-serif text-cyan-400"> {s3} </p>
+                    <p className=" text-xl font-serif text-cyan-400"> {s4} </p>
+                    <p className=" text-xl font-serif text-cyan-400"> {s5} </p>
                 </div>
             </div>
         </>
