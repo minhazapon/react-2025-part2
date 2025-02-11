fc147d1e11b34a133c2e7782ea1c68cb72bd3ee6
minhazapon <minhazaponcodingwarrior@gmail.com>
2025-02-10 20:05:58 -0800

    added file

--- src/event handling/EventHandle.jsx
+++ src/event handling/EventHandle.jsx
@@ -1,8 +1,21 @@
 
 
 function Event() {
+
+
     return (
-        <div>
+        <div className=" m-20 ">
+
+            <div>
+
+                <div className=" border-[1px] border-purple-600 bg-black w-fit p-10 text-white
+                 ">
+                    <p>Minhazul abedin apon</p>
+                    <p>Title: software developer</p>
+                    <p className=" btn mt-5 bg-black  hover:border-purple-400 text-white ">Contact Him</p>
+                </div>
+
+            </div>
 
         </div>
     )
