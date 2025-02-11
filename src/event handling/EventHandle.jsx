396abf15be8b322143e47f22727adcbb45802043
minhazapon <minhazaponcodingwarrior@gmail.com>
2025-02-10 20:10:12 -0800

    added event file

--- src/event handling/EventHandle.jsx
+++ src/event handling/EventHandle.jsx
@@ -2,21 +2,20 @@
 
 function Event() {
 
+    const handleContact = (contact, gmail) => {
+        console.log(`contact with ${contact} and work with him and his gmail is: ${gmail}`)
+    }
 
     return (
         <div className=" m-20 ">
-
             <div>
-
                 <div className=" border-[1px] border-purple-600 bg-black w-fit p-10 text-white
                  ">
                     <p>Minhazul abedin apon</p>
                     <p>Title: software developer</p>
-                    <p className=" btn mt-5 bg-black  hover:border-purple-400 text-white ">Contact Him</p>
+                    <button onClick={() => handleContact("minhaz the react expert", "minhazTheReact@gmail.com")} className=" btn mt-5 bg-black  hover:border-purple-400 text-white ">Contact Him</button>
                 </div>
-
             </div>
-
         </div>
     )
 }
