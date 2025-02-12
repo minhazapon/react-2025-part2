2166d68406faeecf9ef0121f38f0e3fbf348c72e
minhazapon <minhazaponcodingwarrior@gmail.com>
2025-02-12 05:04:07 -0800

    added file

--- src/toggle file/Toggle.jsx
+++ src/toggle file/Toggle.jsx
@@ -5,13 +5,17 @@ function Toggle() {
 
     const [isOpen, setIsOpen] = useState(false)
 
+    const handleToggle = () => {
+        setIsOpen(!isOpen)
+    }
+
     return (
         <>
             <div className=" m-20 ">
                 <div className=" flex justify-center mt-20 ">
-                    <div className=" border-[1px]  h-[100px] w-[300px] p-8 rounded-full bg-black ">
+                    <div className=" border-[1px]  h-[100px] w-[300px] p-8 rounded-full bg-black " onClick={handleToggle}>
                         <div className="  bg-white text-black h-[40px] w-[40px] p-2 rounded-full cursor-pointer  ">
-                            <span className=" font-mono  ml-1 "> {isOpen ? "OFF" : "ON"} </span>
+                            <span className=" font-mono"> {isOpen ? "OFF" : "ON"} </span>
                         </div>
                     </div>
                 </div>
