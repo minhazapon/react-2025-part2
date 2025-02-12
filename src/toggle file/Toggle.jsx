7456cd05468785369b0768179b6d95a48be913c4
minhazapon <minhazaponcodingwarrior@gmail.com>
2025-02-12 05:00:45 -0800

    added file

--- src/toggle file/Toggle.jsx
+++ src/toggle file/Toggle.jsx
@@ -9,9 +9,9 @@ function Toggle() {
         <>
             <div className=" m-20 ">
                 <div className=" flex justify-center mt-20 ">
-                    <div className=" border-[1px]  h-[100px] w-[300px] p-8 rounded-3xl bg-black ">
-                        <div className="  bg-white text-black h-[40px] w-[40px] p-2 rounded-full  ">
-                            <span className=" cursor-pointer"> {isOpen ? "Off" : "on"} </span>
+                    <div className=" border-[1px]  h-[100px] w-[300px] p-8 rounded-full bg-black ">
+                        <div className="  bg-white text-black h-[40px] w-[40px] p-2 rounded-full cursor-pointer  ">
+                            <span className=" font-mono  ml-1 "> {isOpen ? "OFF" : "ON"} </span>
                         </div>
                     </div>
                 </div>
