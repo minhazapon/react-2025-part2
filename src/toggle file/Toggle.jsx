7ced19e12a7b61db3735a089a39e5a0064976192
minhazapon <minhazaponcodingwarrior@gmail.com>
2025-02-12 04:58:19 -0800

    added file

--- src/toggle file/Toggle.jsx
+++ src/toggle file/Toggle.jsx
@@ -1,10 +1,22 @@
+import { useState } from "react"
 
 
 function Toggle() {
-    return (
-        <div>
 
-        </div>
+    const [isOpen, setIsOpen] = useState(false)
+
+    return (
+        <>
+            <div className=" m-20 ">
+                <div className=" flex justify-center mt-20 ">
+                    <div className=" border-[1px]  h-[100px] w-[300px] p-8 rounded-3xl bg-black ">
+                        <div className="  bg-white text-black h-[40px] w-[40px] p-2 rounded-full  ">
+                            <span className=" cursor-pointer"> {isOpen ? "Off" : "on"} </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
 
