a5d585121940912330ac035ed9afa0943a36c937
minhazapon <minhazaponcodingwarrior@gmail.com>
2025-02-12 05:18:41 -0800

    added file

--- src/toggle file/Toggle.jsx
+++ src/toggle file/Toggle.jsx
@@ -14,12 +14,12 @@ function Toggle() {
             <div className=" m-20 ">
                 <div className=" flex justify-center mt-20 ">
                     <div className=" border-[1px]  h-[100px] w-[300px] p-8 rounded-full bg-black " onClick={handleToggle}>
-                        <div className="  bg-white text-black h-[40px] w-[40px] p-2 rounded-full cursor-pointer  ">
-                            <span className=" font-mono"> {isOpen ? "OFF" : "ON"} </span>
+                        <div className={`bg-white text-black h-[40px] w-[40px] p-2 rounded-full cursor-pointer ${isOpen ? "off" : "on"}`} >
+                            <span className=" font-mono"> {isOpen ? "off" : "on"} </span>
                         </div>
                     </div>
                 </div>
-            </div>
+            </div >
         </>
     )
 }
