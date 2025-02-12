e4e1cc75cb9f26f7a7bee9a4ea445993fcafd449
minhazapon <minhazaponcodingwarrior@gmail.com>
2025-02-12 03:30:54 -0800

    added file

--- src/liftState/InputValue.jsx
+++ src/liftState/InputValue.jsx
@@ -4,7 +4,7 @@ function InputValue() {
 
     return (
         <div>
-
+            <input placeholder="Type Here" className=" p-2 h-[50px] w-[500px] border-[1px] border-cyan-300 text-black " type="text" name="text" id="" />
         </div>
     )
 }
