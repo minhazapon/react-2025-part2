b50105b4030cfce6ad6c95debc4a97c79805f428
minhazapon <minhazaponcodingwarrior@gmail.com>
2025-02-13 02:01:11 -0800

    added file

--- src/todo file/Todo.jsx
+++ src/todo file/Todo.jsx
@@ -2,9 +2,22 @@
 
 function Todo() {
     return (
-        <div>
+        <>
+            <div className=" m-20 ">
 
-        </div>
+                <div>
+
+                    <div>
+                        <p className=" text-5xl font-bold text-cyan-700 text-center ">Todo List</p>
+                    </div>
+
+
+
+                </div>
+
+            </div>
+        </>
+
     )
 }
 
