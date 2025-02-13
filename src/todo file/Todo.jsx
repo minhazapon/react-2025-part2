f17ec5d557b4fc993525cf191c140490c0ac9b69
minhazapon <minhazaponcodingwarrior@gmail.com>
2025-02-13 02:09:33 -0800

    added todo file

--- src/todo file/Todo.jsx
+++ src/todo file/Todo.jsx
@@ -11,10 +11,16 @@ function Todo() {
                     <div className=" flex justify-center mt-8 ">
                         <form className=" flex items-center">
                             <div>
-                                <input required className="  p-3 h-[50px] border-[1px] w-[400px] rounded-l-[20px] " placeholder="Add Task" type="text" name="text" id="" />
+                                <input required className="  p-3 h-[50px] border-[1px] w-[400px] rounded-l-[20px] "
+                                    placeholder="Add Task"
+                                    type="text"
+                                    name="text"
+                                    id="" />
                             </div>
                             <div>
-                                <input className=" text-white h-[50px] p-3 rounded-r-[20px] bg-cyan-700 " type="submit" value="AddTask" />
+                                <input className=" text-white h-[50px] p-3 rounded-r-[20px] bg-cyan-700 "
+                                    type="submit"
+                                    value="AddTask" />
                             </div>
                         </form>
                     </div>
